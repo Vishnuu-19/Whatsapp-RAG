3ef7edb3ede713202fe4e53002bb1a6428208d65
vishnu <vishnu192004@gmail.com>
2026-01-16 20:14:30 +0530

    working on another device

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from preprocessing import parse_whatsapp_chat
 from chunking import create_chunks
 
 if __name__ == "__main__":
-    messages = parse_whatsapp_chat(r"C:\Users\vishn\OneDrive\Desktop\Whatsapp-RAG\data\raw\chat.txt")
+    messages = parse_whatsapp_chat(r"data\raw\chat.txt")
     df = pd.DataFrame(messages)
     print("Number of messages: ",len(df))
 
-    chunks = create_chunks(messages)
+    # chunks = create_chunks(messages)
 
-    # Validation code
-    df = pd.DataFrame(chunks)
-    print("Number of chunks: ",len(df))
+    # # Validation code
+    # df = pd.DataFrame(chunks)
+    # print("Number of chunks: ",len(df))
 
     # print(df[df["sender_id"]=="user_001"].shape[0])
     # print(df[df["is_multiline"] == True][["message_id", "raw_lines_count"]])
